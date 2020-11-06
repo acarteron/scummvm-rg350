7d7e9706a2a21361121297b652737e913123f2b3
Adrien Carteron <adrien.carteron@aquilenet.fr>
2020-11-06 10:40:47 +0100

    [FEAT] set library path on runtime

--- dists/gcw0/scummvm.sh
+++ dists/gcw0/scummvm.sh
@@ -6,4 +6,4 @@ if [ ! -f $HOME/.scummvmrc ] ; then
 	cp ./scummvmrc $HOME/.scummvmrc
 fi
 
-exec ./scummvm 2>&1 >/var/tmp/scummvm.log
+exec env LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH ./scummvm 2>&1 >/var/tmp/scummvm.log
