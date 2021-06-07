0b2373a2d2381c92c4cacf9774ffddd351ec32d7
Adrien Carteron <adrien.carteron@aquilenet.fr>
2021-06-07 23:01:50 +0200

    enable twine and monkey4 engine

--- backends/platform/dingux/build.gcw0.sh
+++ backends/platform/dingux/build.gcw0.sh
@@ -3,4 +3,4 @@
 export PATH=/opt/gcw0-toolchain/usr/bin:$PATH
 
 # Enable high resolution engines for 640x480 resolution
-./configure --host=gcw0 --with-mad-prefix=/opt/gcw0-toolchain/mipsel-gcw0-linux-uclibc/sysroot/usr/ --enable-plugins --default-dynamic --enable-release --enable-highres --enable-engine=grim && make -j9 gcw-opk && ls -l scummvm.opk
+./configure --host=gcw0 --with-mad-prefix=/opt/gcw0-toolchain/mipsel-gcw0-linux-uclibc/sysroot/usr/ --enable-plugins --default-dynamic --enable-release --enable-highres --enable-engine=grim,twine,monkey4 && make -j9 gcw-opk && ls -l scummvm.opk
